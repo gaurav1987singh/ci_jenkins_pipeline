685e86d0741f3bea5f7300e3432cc9b5c078df68
singh.gaurav <singh.gaurav@cardinalhealth.com>
2018-08-28 07:50:03 -0400

    Compile error removed in HelloWorld.java

--- sample_javaProject/src/main/java/HelloWorld.java
+++ sample_javaProject/src/main/java/HelloWorld.java
@@ -1,5 +1,5 @@
-Class HelloWorld {
-public static void main(String args[]){  
-     System.out.println("Hello Java");  
+public class HelloWorld {
+public String holla(){  
+    return "Hello Java";  
     }
  }
